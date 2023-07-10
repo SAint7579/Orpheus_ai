8ff7eae802fb2f551428edf299e168b164d5237b
anirudh <anirudhsai027@gmail.com>
2023-07-10 19:07:13 +0200

    new commits

--- page2.js
+++ page2.js
@@ -1,6 +1,15 @@
 const songs=document.getElementById("right_cnt");
 const song_button=document.getElementById("song_btn");
 
+function updateHeading(headingNumber) {
+    const dropdown = document.getElementById(`dropdown${headingNumber}`);
+    const heading = document.getElementById(`heading${headingNumber}`);
+    const selectedSong = dropdown.value;
+    
+    heading.textContent = selectedSong !== '' ? selectedSong : `Default Heading ${headingNumber}`;
+}
+  
+
 song_button.onclick=unblock;
 
 function unblock(){
