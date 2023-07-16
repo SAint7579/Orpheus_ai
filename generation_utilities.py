c52f66d38551742a4e17f4991b4b0f5d7a607b51
SAint7579 <vishwa20@outlook.com>
2023-07-16 15:50:22 +0200

    Minor changes

--- generation_utilities.py
+++ generation_utilities.py
@@ -151,6 +151,9 @@ def generate_songs(conditioning_songs, similarity=0.5, quality=500, merging_qual
     ## Merging songs
     print("Merging songs...")
     if len(conditioning_songs)>1:
+        print(conditioning_songs)
+        for c in conditioning_songs:
+            print(c.shape)
         spec_merged, merged = merge_songs(conditioning_songs, ddim, slerp_steps=merging_quality, diffusion_steps=merging_quality, device=device)
     else:
         merged = conditioning_songs[0]
